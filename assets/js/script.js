5e2e52ffef45e1c76255a33019a4fd369091ff65
elliottdombrowski <elliottroyal@me.com>
2021-09-12 21:01:08 -0500

    added button hovers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,20 @@ var aboutNav = $("#about-button-nav");
 var workNav = $("#work-button");
 var contactNav = $("#contact-button");
 var downButton = $("#about-jump");
+var bell = $("#bell");
+var media = $("#media-display");
+var bell2 = $("#trymediatwo");
 
 
 //Adds event listeners for nav buttons
 downButton.on("click", showAboutAlt);
 aboutNav.on("click", showAbout);
 workNav.on("click", showWork);
+// bell.on("click", showMedia);
 
+bell.first().click(function() {
+    media.first().fadeToggle(2000, "linear");
+});
 
 //On respective button click, sets visibility for respective section.
 function showAbout() {
@@ -25,3 +32,7 @@ function showWork() {
     aboutSection.css("display", "flex");
     workSection.css("display", "flex");
 }
+
+// function showMedia () {
+//     // media.css("display", "block");
+// }
