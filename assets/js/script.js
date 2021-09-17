15c66f5da8e1a9f9699cdf9314035ff14a9f7be7
elliottdombrowski <elliottroyal@me.com>
2021-09-17 12:33:14 -0500

    playing with timers for slideshow feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var aboutCont = $(".about-container");
-var aboutNav = $("#about-button-nav");
 var media = $("#media-display");
 
 var pages = [
@@ -10,6 +9,7 @@ var pages = [
 ]
 
 //Adds event listeners for buttons
+var currIndex = 0;
 
 //fades in/out hidden media link nav bar
 var bell = $("#bell");
@@ -18,33 +18,65 @@ bell.first().click(function() {
 });
 
 //fades out main page to about page
-var downButton = $("#about-button");
-downButton.click(function() {
-    homeSection.fadeOut(2000);
-    //waits until home page animation is finished to start about page animation
-    homeSection.promise().done(function(){
-        aboutSection.fadeIn(2000);
+// var downButton = $("#about-button");
+// downButton.click(function() {
+//     pages[currIndex].fadeOut(2000);
+//     //waits until home page animation is finished to start about page animation
+//     pages[currIndex].promise().done(function(){
+//         aboutSection.fadeIn(2000);
+//         currIndex = 1;
+//     });
+// });
+
+function toSection(index) {
+    pages[currIndex].fadeOut(2000);
+    pages[currIndex].promise().done(function() {
+        pages[index].fadeIn(2000);
+        currIndex = index;
     });
+}
+
+var aboutNav = $("#about-button-nav");
+aboutNav.click(function() {
+    toSection(1);
 });
 
 //fades out page to work page
 var workNav = $("#work-button");
 workNav.click(function() {
-    aboutSection.fadeOut(2000);
-    //waits until cur page animation is finished
-    aboutSection.promise().done(function() {
-        workSection.fadeIn(2000);
-    });
+    toSection(2);
 });
 
 //fades out page to contact page
 var contactNav = $("#contact-button");
 contactNav.click(function() {
-    workSection.fadeOut(2000);
-    workSection.promise().done(function() {
-        contactSection.fadeIn(2000);
-    });
+    toSection(3);
 });
+
+//slideshow function
+var downButton = $("#about-button");
+var timer = 10;
+// downButton.click(setInterval(function() {
+//     timer--;
+//     console.log(timer);
+
+//     if (timer === 0) {
+//         clearInterval();
+//     }
+// }, 1000));
+downButton.click(slideShow())
+
+function slideShow() {
+    setInterval(function() {
+        timer--;
+        console.log(timer);
+
+        if (timer >= 0) {
+            clearInterval();
+            console.log("timer done");
+        }
+    }, 1000);
+}
 
 //flashing effect on about page down 
 var aboutDown = $("#about-down");
