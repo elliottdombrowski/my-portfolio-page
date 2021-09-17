112af4a92a2aa401e475747857ec3650822ad9f5
elliottdombrowski <elliottroyal@me.com>
2021-09-16 19:29:29 -0500

    added work page carousel and about page layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
-var aboutSection = $("#about-page");
 var aboutCont = $(".about-container");
-var homeSection = $("#home-page");
-var workSection = $("#work-page");
-var contactSection = $("#contact-page");
 var aboutNav = $("#about-button-nav");
-var workNav = $("#work-button");
 var contactNav = $("#contact-button");
-var downButton = $("#about-button");
-var aboutDown = $("#about-down");
-var bell = $("#bell");
 var media = $("#media-display");
 
+var pages = [
+    homeSection = $("#home-page"),
+    aboutSection = $("#about-page"),
+    workSection = $("#work-page"),
+    contactSection = $("#contact-page")
+]
 
 //Adds event listeners for buttons
 
 //fades in/out hidden media link nav bar
+var bell = $("#bell");
 bell.first().click(function() {
-    media.first().fadeToggle(2000, "linear");
+    media.first().fadeToggle(1000, "linear");
 });
 
 //fades out main page to about page
+var downButton = $("#about-button");
 downButton.click(function() {
     homeSection.fadeOut(2000);
     //waits until home page animation is finished to start about page animation
@@ -28,9 +28,18 @@ downButton.click(function() {
     });
 });
 
-//fades out 
+//fades out page to work page
+var workNav = $("#work-button");
+workNav.click(function() {
+    aboutSection.fadeOut(2000);
+    //waits until cur page animation is finished
+    aboutSection.promise().done(function() {
+        workSection.fadeIn(2000);
+    });
+});
 
-//flashing effect on about page down btn
+//flashing effect on about page down 
+var aboutDown = $("#about-down");
 $(document).ready(() => {
     setInterval(() => {
         aboutDown.fadeIn("slow", "linear");
