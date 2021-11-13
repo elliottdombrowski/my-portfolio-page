1255dda6f5c9939ec1507a5d492dae06b2e16c80
elliottdombrowski <elliottroyal@me.com>
2021-11-13 12:45:39 -0600

    Refactored email validation to use regex. TODO - FIX JQUERY IN CONTACTJS

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,4 +1,10 @@
-const alertMsg = document.getElementById('response-msg'); 
+const alertMsg = document.getElementById('response-msg');
+
+//VALIDATE EMAIL W/ REGEX
+function validateEmail(valEmail) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(valEmail);
+}
 
 //PROB CAN REFACTOR THIS TO USE FOR LOOP LATER
 const validateContactForm = async (event) => {
@@ -12,15 +18,18 @@ const validateContactForm = async (event) => {
     
     alertMsg.innerHTML = '';
     if (!valName) {
-        console.log('no name');
+        alertMsg.fadeIn(2000);
         alertMsg.innerHTML = 'please enter your name.'
 
         //REFACTOR EMAIL VALIDATION LATER
-    } else if (!valEmail || !valEmail.includes('@') && ".com") {
+    } else if (!validateEmail(valEmail)) {
+        alertMsg.fadeIn(2000);
         alertMsg.innerHTML = 'please enter a valid email.'
     } else if (!valMsg) {
+        alertMsg.fadeIn(2000);
         alertMsg.innerHTML = 'please leave a message.'
     } else {
+        alertMsg.fadeIn(2000);
         alertMsg.innerHTML = 'thanks.'
     };
 };
